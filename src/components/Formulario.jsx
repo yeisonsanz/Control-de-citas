d3c8a2602b3539756386cf7d92680832a0a51f37
Yeison Estiven Sanz Castro <yecastro1@stefanini.com>
2022-12-06 23:09:20 -0500

    iniciando el proyecto con tailwin

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Formulario = () => {
+  return (
+    <h2>Formulario</h2>
+  )
+}
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ListadoPacientes = () => {
+  return (
+    <h2>ListadoPacientes</h2>
+  )
+}
